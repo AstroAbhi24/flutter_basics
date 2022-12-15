f40f845b03366608951e33375376323f034dcfb3
Astro_Abhi24 <abhishek.it004@bppimt.ac.in>
2022-12-15 22:37:51 +0530

    questions widget added

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
+import './questions.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _quesIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _quesIndex = _quesIndex + 1;
+    });
+    print(_quesIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    void answerQuestion(){
-      print('Button pressed');
-    }
     var questions = [
       'Who is the name of the Presient of India?',
-      'Who is the richest man of the World?'
+      'Who is the richest man of the World?',
+      'What is my name?'
     ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello World"),
         ),
-        body: Column(
-          children: [
-            Text('The question!'),
-            ElevatedButton(
-              onPressed: answerQuestion,
-              child: Text(
-                "answer 1",
+        body: Align(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text(questions[_quesIndex]),
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: Text(
+                  "answer 1",
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: answerQuestion,
-              child: Text("answer 2"),
-            ),
-            ElevatedButton(
-              onPressed: answerQuestion,
-              child: //Text("answer 3"),
-              (){
-                print("Answer 3"),
-              },
-            ),
-          ],
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: Text("answer 2"),
+              ),
+              ElevatedButton(
+                onPressed: answerQuestion,
+                child: Text("answer 3"),
+              ),
+            ],
+          ),
         ),
       ),
     );
